bd83b59d5f17e76dc34739ae1444a9d0b5293554
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-07-03 20:31:36 -0700

    Update gitscon.h

--- gitscon.h
+++ gitscon.h
@@ -8,7 +8,9 @@ void cd(const char * directory);
 void mkdir(const char * directory){ directory = new const char; };
 
 #define GITSCON_VERSION "1.0"
- 
+
+const char * GITSCON_NAME = "Git Scon 1.0...\n";
+
 #define gscon_mkdir ( mkdir("%c") )
 #define gscon_cd ( cd("%c") ) 
 
