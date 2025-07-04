77393681cd34038f586c9bae22b05d5b7c750edc
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-07-03 20:27:26 -0700

    Update gitscon.h

--- gitscon.h
+++ gitscon.h
@@ -2,13 +2,14 @@
 #define GITSCON_H
 
 //gitscon.h
+//gitscon.h copyright ( c ) UncStatus++
 
 void cd(const char * directory);
 void mkdir(const char * directory){ directory = new const char; };
 
 #define GITSCON_VERSION "1.0"
-
-#define gscon_mkdir (mkdir("%c"))
-#define gscon_cd ("%c") 
+ 
+#define gscon_mkdir ( mkdir("%c") )
+#define gscon_cd ( cd("%c") ) 
 
 #endif
