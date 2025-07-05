380a12f816f7e076e1146a9ac9ecfd8afe064713
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-07-05 10:33:25 -0500

    Update gitscon.h

--- gitscon.h
+++ gitscon.h
@@ -4,14 +4,28 @@
 //gitscon.h
 //gitscon.h copyright ( c ) UncStatus++
 
+#define GITSCON_VERSION "1.0"
+
 void cd(const char * directory);
 void mkdir(const char * directory){ directory = new const char; };
 
-#define GITSCON_VERSION "1.0"
-
-const char * GITSCON_NAME = "Git Scon 1.0...\n";
+const char* GITSCON_NAME = "Git Scon 1.0...\n";
 
 #define gscon_mkdir ( mkdir("%c") )
-#define gscon_cd ( cd("%c") ) 
+#define gscon_cd ( cd("%c") )
+
+//
+//
+//*!GITSCON GTK SUPPORT SECTION!*
+//
+//
+
+inline bool gitscon_prj_chck_gtk_included(const char* prjfcode){
+      if( prjfcode = "#include <gtk/gtk.h>" ){
+          printf
+      }
+}
+
+
 
 #endif
